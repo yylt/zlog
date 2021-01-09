d93108091d88717e2057fad262e7bf4235dd1d75
yang.yang <yang.yang@easystack.cn>
2021-01-09 11:50:01 +0800

    init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/yylt/zlog
+
+go 1.15
+
+require (
+	github.com/coredns/caddy v1.1.0
+	github.com/coredns/coredns v1.8.0
+	github.com/miekg/dns v1.1.35
+	go.uber.org/zap v1.16.0
+)
---
+++ setup.go
@@ -0,0 +1,121 @@
+package zlog
+
+import (
+	"go.uber.org/zap/zapcore"
+	"os"
+	"strings"
+
+	"github.com/coredns/caddy"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/pkg/replacer"
+	"github.com/coredns/coredns/plugin/pkg/response"
+	"github.com/miekg/dns"
+	"go.uber.org/zap"
+)
+
+const (
+	pluginname = "zlog"
+)
+
+func init() { plugin.Register(pluginname, setup) }
+
+func setup(c *caddy.Controller) error {
+	rules, err := logParse(c)
+	if err != nil {
+		return plugin.Error("log", err)
+	}
+
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		return Zlogger{Next: next, Rules: rules, repl: replacer.New(), logger: defaultLog()}
+	})
+
+	return nil
+}
+
+func logParse(c *caddy.Controller) ([]Rule, error) {
+	var rules []Rule
+
+	for c.Next() {
+		args := c.RemainingArgs()
+		length := len(rules)
+
+		switch len(args) {
+		case 0:
+			// Nothing specified; use defaults
+			rules = append(rules, Rule{
+				NameScope: ".",
+				Format:    DefaultLogFormat,
+				Class:     make(map[response.Class]struct{}),
+			})
+		case 1:
+			rules = append(rules, Rule{
+				NameScope: dns.Fqdn(args[0]),
+				Format:    DefaultLogFormat,
+				Class:     make(map[response.Class]struct{}),
+			})
+		default:
+			// Name scopes, and maybe a format specified
+			format := DefaultLogFormat
+
+			if strings.Contains(args[len(args)-1], "{") {
+				switch args[len(args)-1] {
+				case "{common}":
+					format = CommonLogFormat
+				case "{combined}":
+					format = CombinedLogFormat
+				default:
+					format = args[len(args)-1]
+				}
+
+				args = args[:len(args)-1]
+			}
+
+			for _, str := range args {
+				rules = append(rules, Rule{
+					NameScope: dns.Fqdn(str),
+					Format:    format,
+					Class:     make(map[response.Class]struct{}),
+				})
+			}
+		}
+
+		// Class refinements in an extra block.
+		classes := make(map[response.Class]struct{})
+		for c.NextBlock() {
+			switch c.Val() {
+			// class followed by combinations of all, denial, error and success.
+			case "class":
+				classesArgs := c.RemainingArgs()
+				if len(classesArgs) == 0 {
+					return nil, c.ArgErr()
+				}
+				for _, c := range classesArgs {
+					cls, err := response.ClassFromString(c)
+					if err != nil {
+						return nil, err
+					}
+					classes[cls] = struct{}{}
+				}
+			default:
+				return nil, c.ArgErr()
+			}
+		}
+		if len(classes) == 0 {
+			classes[response.All] = struct{}{}
+		}
+
+		for i := len(rules) - 1; i >= length; i-- {
+			rules[i].Class = classes
+		}
+	}
+
+	return rules, nil
+}
+
+func defaultLog() *zap.Logger {
+	console := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{MessageKey: "M"})
+	ws := zapcore.AddSync(os.Stdout)
+	lvl := zapcore.DebugLevel
+	return zap.New(zapcore.NewCore(console, ws, lvl))
+}
